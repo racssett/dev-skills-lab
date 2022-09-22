bda1f43116369bb1a2921905152dca14f5b17054
racssett <rachel.c.hassett@gmail.com>
2022-09-22 15:59:54 -0500

    Refactor controller to accept data from model

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,8 +1,15 @@
-import { skills } from '../data/skill-data.js'
+import { Skill } from '../models/skill.js'
 
 function index(req, res) {
-    res.render('skills/index', {
-        skills: skills
+    Skill.find({})
+    .then(skills => {
+        res.render('skills/index', {
+            skills: skills,
+        })
+    })
+    .catch(error => {
+        console.log(error)
+        res.redirect('/')
     })
 }
 
