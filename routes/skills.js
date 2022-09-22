d8595089f3c91e64e7ee0664e9adffcb17b06ec6
racssett <rachel.c.hassett@gmail.com>
2022-09-22 14:36:31 -0500

    Setup skills route and HTML

--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
+// import skills data
+import { skills } from '../data/skill-data.js'
 
 const router = Router()
 
-/* GET users listing. */
+/* GET skills listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource')
+  res.render('skills/index', {
+    skills: skills
+  })
 })
 
 export {
