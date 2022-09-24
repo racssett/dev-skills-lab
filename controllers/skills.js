1c2c04155b1690518a9b1c95f3c42bddf9640590
racssett <rachel.c.hassett@gmail.com>
2022-09-24 01:21:00 -0500

    Add delete skill functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,17 +2,17 @@ import { skills } from '../data/skill-data.js'
 import { Skill } from '../models/skill.js'
 
 function index(req, res) {
-    Skill.find({})
-    .then(skills => {
-        res.render('skills/index', {
-            skills: skills,
-            date: req.date
-        })
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      skills: skills,
+      date: req.date
     })
-    .catch(error => {
-        console.log(error)
-        res.redirect('/')
-    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
 }
 
 function newSkill(req, res) {
@@ -20,33 +20,45 @@ function newSkill(req, res) {
 }
 
 function create(req, res) {
-    console.log(req.body)
-    Skill.create(req.body)
-    .then(skill => {
-        res.redirect('/skills')
-    })
-    .catch(error => {
-        console.log(error)
-        res.redirect('/skills')
-    })
+  console.log(req.body)
+  Skill.create(req.body)
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
 }
 
 function show(req, res) {
-    Skill.findById(req.params.id)
-    .then(skill => {
-        res.render('skills/show', {
-            skill: skill,
-        })
+  Skill.findById(req.params.id)
+  .then(skill => {
+    res.render('skills/show', {
+      skill: skill,
     })
-    .catch(error => {
-        console.log(error)
-        res.redirect('/skills')
-    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
+}
+
+function deleteSkill(req, res) {
+  Skill.findByIdAndDelete(req.params.id)
+  .then(skill=> {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/skills')
+  })
 }
 
 export {
-    index,
-    newSkill as new,
-    create,
-    show,
+  index,
+  newSkill as new,
+  create,
+  show,
+  deleteSkill as delete,
 }
